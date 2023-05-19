8439fd66c0208a9cf8282bf9a27e8681250829d6
Arsene Rurangiza <arurangi@shi-r9-p4.s19.be>
2023-05-19 15:51:35 +0200

    updated parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akorompa <akorompa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 16:10:32 by akorompa          #+#    #+#             */
-/*   Updated: 2023/05/17 13:42:11 by akorompa         ###   ########.fr       */
+/*   Updated: 2023/05/19 15:49:46 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	errormsg(char *type, int code)
+{
+	printf("Error: ");
+	printf("%s\n",type);
+	return (code);
+}
 
 int	file_size(int fd)
 {
@@ -75,6 +82,115 @@ char	**get_file(char *map, t_data *data)
 	return (file);
 }
 
+int	is_start_of_map(char **map, int i)
+{
+	if (!ft_strncmp("01", map[i], 2) || !ft_strncmp("10", map[i], 2) || !ft_strncmp("11", map[i], 2))
+		return (1);
+	return (0);
+			
+}
+
+int	invalid_characters(char *str)
+{
+	printf("---> Checking: invalid characters\n");
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] != '0'
+			&& str[i] != '1'
+			&& str[i] != 'N'
+			&& str[i] != 'S'
+			&& str[i] != 'E'
+			&& str[i] != 'W'
+			&& str[i] != ' ')
+		{
+			printf("Error: invalid character (%c) at col [%d]\n", str[i], i);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	find_last_row(char **map, int i)
+{
+	while (map[i])
+		i++;
+	return (i);
+}
+
+#define ERROR 1
+#define SUCCESS 0
+
+int	not_surrounded_by_walls(char *row, int i, int first_row_index, int last_row_index)
+{
+	int k = 0;
+	
+	printf("---> Checking: surrounded by walls\n");
+	while (row[k] && row[k] == ' ')
+		k++;
+
+	if (i == first_row_index || i == last_row_index)
+	{
+		while (row[k]) {
+			if (row[k] != '1')
+				return (errormsg("not surrounded by walls", ERROR));
+		}
+	}
+	else
+	{
+		if (row[k] != '1')
+			return (errormsg("not surrounded by walls", ERROR));
+		while (row[k])
+			k++;
+		if (row[k - 1] != '1')
+			return (errormsg("not surrounded by walls", ERROR));
+	}
+
+	while (row[k])
+	{
+		if (row[k] != ' ')
+			return (errormsg("not surrounded by walls", ERROR));
+		k++;
+	}
+
+	return (SUCCESS);
+
+	// if ((row[0] && row[0] == '1') && (row[last] && row[last] == '1'))
+	// return (1);
+	
+}
+
+int	invalid_map(char **map)
+{
+	// Map is composed of only 6 possible characters
+	// 0 1 N S E W
+	printf("---> Checking if map is valid\n");
+	int i = 0, first_row, last_row;
+	while (map[i])
+	{
+		// Find start of map
+		if (is_start_of_map(map, i))
+		{
+			printf("---> Found start of map\n");
+			first_row = i;
+			last_row = find_last_row(map, i);
+			// Traverse it
+			while (map[i])
+			{
+				if (invalid_characters(map[i])
+					|| not_surrounded_by_walls(map[i], i, first_row, last_row))
+					return (1);
+				// check for valid characters
+				i++;
+			}
+			return (0);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	parsing(t_data *data, char *map)
 {
 	char	**file;
@@ -82,6 +198,13 @@ int	parsing(t_data *data, char *map)
 	file = get_file(map, data);
 	if (!file)
 		return (0);
+	if (invalid_map(file))
+		return (errormsg("Invalid map", 0));
+	for (int i = 0; file[i] != NULL; i++)
+		printf("%s", file[i]);
+	
+	exit(0);
+
 	data->map = get_map(file, data);
 	get_colors(data, file);
 	get_texture(data, file);
